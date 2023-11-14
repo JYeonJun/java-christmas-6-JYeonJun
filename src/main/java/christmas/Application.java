a83d630d2c60b3cbb0666fef0cfabaeda12582f5
JYeonJun <97449471+JYeonJun@users.noreply.github.com>
2023-11-15 01:33:26 +0900

    feat: 크리스마스 프로모션 프로젝트 구현 완료

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -1,7 +1,31 @@
 package christmas;
 
+import controller.ChristmasController;
+import java.util.List;
+import model.Customer;
+import model.Menu;
+import model.Receipt;
+
 public class Application {
+    private static final ChristmasController christmasController = new ChristmasController();
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        processChristmasEvent();
+    }
+
+    private static void processChristmasEvent() {
+        christmasController.showWelcomeMessage();
+
+        int expectedVisitDate = christmasController.getExpectedVisitDate();
+        List<Menu> orderMenus = christmasController.getUserMenuSelection();
+
+        Customer customer = createCustomer(orderMenus, expectedVisitDate);
+
+        christmasController.showEventRewardsPreview(customer);
+    }
+
+    private static Customer createCustomer(List<Menu> orderMenus, int expectedVisitDate) {
+        Receipt receipt = christmasController.createReceipt(orderMenus, expectedVisitDate);
+        return christmasController.createCustomer(orderMenus, receipt);
     }
 }
